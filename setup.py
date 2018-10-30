d1bf6c1b2a8c50f139624668a3a1470f12550e26
Marius Iversen <pillus@chasenet.org>
2018-10-30 17:28:36 +0100

    added support for installing modules

---
+++ setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+from distutils.core import setup
+
+setup(name='arcsightrequesttracker',
+      version='1.0',
+      description='ArcSight Request Tracker Integration',
+      author='Marius Iversen',
+      author_email='marius@chasenet.org',
+      url='https://github.com/P1llus/arcsight-esm-requesttracker',
+      packages=['arcsightesm', 'requesttracker'],
+)
